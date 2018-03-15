fbe23662bf7a0622b490376802ef722795e7d9e2
andersonbui <andersonb@hotmail.es>
2018-03-14 20:22:03 -0500

    reorganizacion directorios

--- src/main/Drogas.java
+++ src/main/Drogas.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package algoritmosmineria;
+package main;
 
 import algoritmosAgrupamiento.Evaluacion;
 import algoritmosAgrupamiento.Naivayes;
